ab899bbd4d6be4ad703c873b1dbe0224c8d9a377
ccloveak <superstarzsw@gmail.com>
2022-02-19 14:34:51 +0900

    feat: add github action

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,7 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('"ログイン"は表示しています', () => {
   render(<App />);
   expect(screen.getByText("ログイン")).toBeInTheDocument();
 });
